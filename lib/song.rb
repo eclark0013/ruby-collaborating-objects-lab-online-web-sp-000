1d2fb5333adc6dd652e8ce7758a3d7001c04fd9a
Eric Clark <eclark0013@gmail.com>
2019-07-03 18:22:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,14 +11,23 @@ class Song
     import=self.new(name)
 
     song_artist=file_name.split(" - ")[0]
+<<<<<<< HEAD
     Artist.new(song_artist).save[0].add_song(import)
     import
+=======
+    artist_instance=Artist.new(song_artist)
+    artist_instance.add_song(import)
+>>>>>>> bc4619bae3cbb56b184c31f5f2d5b5c5d2b3c88e
 #    import.artist=file_name.split(" - ")[0]
 #    import.artist=file_name[0]
 
 #    import.genre=file_name[2]
   end
 
+<<<<<<< HEAD
 #  binding.pry
+=======
+  binding.pry
+>>>>>>> bc4619bae3cbb56b184c31f5f2d5b5c5d2b3c88e
 
 end
